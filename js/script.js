d250dd5ae7e376bd439058ddac2c35c50761108f
Krasivaya <semwagacarine@gmail.com>
2019-07-14 18:40:22 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,27 @@
+function mov() {
+    document.getElementById("m1").style.display = "block";
+    document.getElementById("m2").style.display = "block";
+    document.getElementById("m3").style.display = "block";
+    document.getElementById("m4").style.display = "block";
+    document.getElementById("c1").style.display = "none";
+    document.getElementById("c2").style.display = "none";
+    document.getElementById("c3").style.display = "none";
+    document.getElementById("c4").style.display = "none";
+}
+function cat() {
+    document.getElementById("c1").style.display = "block";
+    document.getElementById("c2").style.display = "block";
+    document.getElementById("c3").style.display = "block";
+    document.getElementById("c4").style.display = "block";
+    document.getElementById("m1").style.display = "none";
+    document.getElementById("m2").style.display = "none";
+    document.getElementById("m3").style.display = "none";
+    document.getElementById("m4").style.display = "none";
+}
+
+function mouseOver() {
+    document.getElementById("wc4").style.display = "block";
+}
+function mouseOut() {
+    document.getElementById("wc4").style.display = "block";
+}
